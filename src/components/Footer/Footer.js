237703d9d106b02dd2590569493ae014c221024a
David Healy <drtjprdave@gmail.com>
2022-11-18 14:42:07 -0700

    Offcanvas format updated.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,8 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
+import "./Footer.scss";
+
 function Footer() {
   return (
     <>
@@ -17,34 +19,39 @@ function Footer() {
           fixed="bottom"
           className="navbar mb-0"
         >
-          <Container fluid>
-            <Navbar.Brand href="#">Layers and Tools</Navbar.Brand>
+          <Container className="">
+            <Navbar.Brand href="#" className="brand p-4 fs-1">
+              Layers and Tools
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+              placement="bottom"
+              className=""
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
+                  Layers and tools
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 ms-0 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+              <Offcanvas.Body className="offCanvasBody">
+                <Nav className="justify-content-end flex-grow-1 ms-0 pe-3 m-1">
+                  <Nav.Link href="#action1">Map Layers</Nav.Link>
+                  <Nav.Link href="#action2">Hunting Layers</Nav.Link>
                   <NavDropdown
-                    title="Dropdown"
+                    title="Pin Points"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action3">Access</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Deer</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action5">
-                      Something else here
+                      Downed Animals
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action6">
+                      Drop New Pin
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import ListGroup from "react-bootstrap/ListGroup";
+import Figure from "react-bootstrap/Figure";
+
+// Imported Profile image
+import mockProfileImage from "./Assets/mockProfileImage.jpg";
 
 const Nav = ({ name, ...props }) => {
   const [show, setShow] = useState(false);
@@ -19,8 +24,20 @@ const Nav = ({ name, ...props }) => {
           <Offcanvas.Title>Hunting Buddy</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <ListGroup variant="flush">
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={mockProfileImage}
+              />
+              <Figure.Caption>DAvid Healy</Figure.Caption>
+            </Figure>
+            <ListGroup.Item>David Healy</ListGroup.Item>
+            <ListGroup.Item>Account Information</ListGroup.Item>
+            <ListGroup.Item>UPGRADE</ListGroup.Item>
+          </ListGroup>
         </Offcanvas.Body>
       </Offcanvas>
     </>
